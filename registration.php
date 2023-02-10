55ca4eebcf50c3ed3efaa09bafd49ddbb83edf0c
uday-kgkrunch <124994964+uday-kgkrunch@users.noreply.github.com>
2023-02-10 16:05:09 +0530

    Delete registration.php

--- registration.php
+++
@@ -1,7 +0,0 @@
-<?php
- 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Kgkrunch_ExlcudeCanceldorder',
-    __DIR__
-);
